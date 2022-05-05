206f3354d7f5876922c1174e54e6f722eb6df00c
valeriocappuccio-bud <valerio.cappuccio@thisisbud.com>
2022-05-05 22:37:20 +0100

    pull requests working

--- experiment.py
+++ experiment.py
@@ -2,11 +2,21 @@ from github import Github, GithubException
 from github.Repository import Repository
 import os
 import json
-from typing import Any, List, Union
-from random import choices as random_choices
+from typing import Any, List, Union, Dict
+import random
 from datetime import datetime
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 gh_client = Github(os.getenv('ACCESS_TOKEN'))
+GhError = Union[str, List, List[Dict]]
+
+PR_BODY = '''
+## Motivation and Context
+
+Making an experiment of making PRs via python script.
+'''
 
 
 def _one(obj: Union[List[Any], Any]) -> Any:
@@ -17,46 +27,94 @@ def get_repo(repo_name: str) -> Repository:
     return _one(gh_client.get_repo(repo_name))
 
 
+def check_status(exc: GithubException):
+    if exc.status != 422:
+        raise
+
+
+def check_already_exists(error: GhError, expected_pattern: str = 'already exist'):
+    if isinstance(error, list):
+        if len(error) > 1:
+            raise
+        message = error[0]['message']
+    elif isinstance(error, dict):
+        message = error['message']
+    elif isinstance(error, str):
+        message = error
+    else:
+        raise
+    if expected_pattern not in message:
+        raise
+
+
 def get_file_content(repo_name: str, file_path: str) -> str:
     file = _one(get_repo(repo_name).get_contents(file_path))
     return file.decoded_content.decode()
 
 
-def create_file(repo_name: str, file_path: str, **kwargs):
-    repo = get_repo(repo_name)
-    response = repo.create_file(file_path, **kwargs)
-    return response
-
-
-def update_file(repo_name: str, file_path: str, **kwargs):
+def update_file(repo_name: str, file_path: str, branch: str = 'main', **kwargs):
     repo = get_repo(repo_name)
     try:
-        return repo.create_file(file_path, **kwargs)
+        resp = repo.create_file(file_path, **kwargs)
     except GithubException as exc:
-        if exc.status == 422:
-            to_update = _one(repo.get_contents(file_path))
-            return repo.update_file(to_update.path, sha=to_update.sha, **kwargs)
-        else:
-            raise exc
+        check_status(exc)
+        check_already_exists(exc.data, '"sha" wasn\'t supplied')
+        to_update = _one(repo.get_contents(file_path, ref=branch))
+        resp = repo.update_file(to_update.path, sha=to_update.sha, branch=branch, **kwargs)
+    logger.info(f'Updated repo: {resp}')
+    return resp
 
 
-def rand_select(data: List) -> List:
-    return random_choices(data, k=int(len(data) * 0.8))
+def create_branch(repo_name: str, branch_name: str, source_branch: str = 'main'):
+    repo = get_repo(repo_name)
+    source = repo.get_branch(source_branch)
+    try:
+        response = repo.create_git_ref(ref='refs/heads/' + branch_name, sha=source.commit.sha)
+        logger.info(f"Created branch: {response}")
+    except GithubException as exc:
+        check_status(exc)
+        check_already_exists(exc.data)
+        logger.info(f"Branch already exists: {branch_name}")
 
 
-def copy_paste_content():
-    content = json.loads(
-        get_file_content('thisisbud/backend-enrichment-sot', 'data/merchants/uk/v1.json')
-    )
-    response = update_file(
-        'valeriocappuccio-bud/pygithub-experiment',
-        'merchants.json',
-        message=f"experiment @ {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}",
-        content=json.dumps(rand_select(content), indent=2),
-    )
-    print(f'Updated repo: {response}')
+def create_pull_request(
+    repo_name: str, head: str, base: str = 'main', title: str = 'New Pull Request', body: str = ""
+):
+    try:
+        get_repo(repo_name).create_pull(title=title, head=head, base=base, body=body)
+        logger.info(f"Made Pull Request for {head}")
+    except GithubException as exc:
+        check_status(exc)
+        check_already_exists(exc.data['errors'])
+        logger.info(f"Pull Request already exists for {head}")
 
 
 if __name__ == "__main__":
-    repo = get_repo('valeriocappuccio-bud/pygithub-experiment')
+    TARGET_REPO = 'valeriocappuccio-bud/pygithub-experiment'
+    BASE_BRANCH = 'main'
+    UPDATED_AT = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    FEATURE_BRANCH = f'update/{UPDATED_AT[:-9]}'
+
+    # example of fetching data
+    content = json.loads(
+        get_file_content('thisisbud/backend-enrichment-sot', 'data/merchants/uk/v1.json')
+    )
+    new_content = json.dumps(random.choices(content, k=10), indent=2)
+
+    create_branch(TARGET_REPO, FEATURE_BRANCH, BASE_BRANCH)
+    update_file(
+        TARGET_REPO,
+        'merchants.json',
+        message=f"experiment @ {UPDATED_AT}",
+        content=new_content,
+        branch=FEATURE_BRANCH,
+    )
+    create_pull_request(
+        TARGET_REPO,
+        head=FEATURE_BRANCH,
+        base=BASE_BRANCH,
+        title=f"Merchants Update: {UPDATED_AT}",
+        body=PR_BODY,
+    )
+
     pass
